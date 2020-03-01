21d9f110bd32123889cff55712f71adaf709e0bf
kmira <kmira@mz.co.kr>
2020-03-01 21:16:37 +0900

    base search 추가

---
+++ src/main/java/io/cloudrium/sample/board/api/model/BaseSearch.java
@@ -0,0 +1,14 @@
+package io.cloudrium.sample.board.api.model;
+
+import org.springframework.data.domain.Sort;
+
+public class BaseSearch {
+
+    private int offset;
+
+    private int limit;
+
+    private Sort sort;
+
+
+}
